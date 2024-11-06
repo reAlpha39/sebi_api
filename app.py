fb87cf255931c8b20205748f696c53e59e332a57
reAlpha39 <yunusw718@gmail.com>
2024-11-06 12:48:47 +0700

    chore: update cors

--- app.py
+++ app.py
@@ -3,29 +3,33 @@ from flask_cors import CORS
 from routers import user
 
 app = Flask(__name__)
-CORS(app, resources={
-    r"/*": {
-        "origins": ["https://porcalabs.github.io"],
-        "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-        "allow_headers": [
-            "Origin",
-            "Content-Type",
-            "Authorization",
-            "Accept",
-            "X-Requested-With",
-            "Access-Control-Request-Method",
-            "Access-Control-Request-Headers",
-            "Access-Control-Allow-Methods"
-        ],
-        "expose_headers": [
-            "Access-Control-Allow-Origin",
-            "Access-Control-Allow-Methods",
-            "Access-Control-Allow-Headers"
-        ],
-        "supports_credentials": True,
-        "max_age": 3600
+CORS(app,
+    resources={
+        r"/api/*":
+        {
+            "origins": ["https://porcalabs.github.io"],
+            "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+            "allow_headers": ["*"],
+            "expose_headers": ["*"],
+            "supports_credentials": True,
+            "send_wildcard": False,
+            "max_age": 3600
+        }
     }
-})
+)
+
+
+@app.before_request
+def handle_preflight():
+    if request.method == "OPTIONS":
+        response = make_response()
+        response.headers.add("Access-Control-Allow-Origin",
+                             "https://porcalabs.github.io")
+        response.headers.add("Access-Control-Allow-Headers", "*")
+        response.headers.add("Access-Control-Allow-Methods",
+                             "GET, POST, PUT, DELETE, OPTIONS")
+        response.headers.add("Access-Control-Max-Age", "86400")
+        return response
 
 # Register blueprints
 app.register_blueprint(user.bp)
