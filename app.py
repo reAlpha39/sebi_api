d77be6c4dddcfa0bc1bcf6be1a9c2d71c8375bcb
reAlpha39 <yunusw718@gmail.com>
2024-11-06 12:39:40 +0700

    chore: update cors

--- app.py
+++ app.py
@@ -4,10 +4,26 @@ from routers import user
 
 app = Flask(__name__)
 CORS(app, resources={
-    r"/api/*": {
+    r"/*": {
         "origins": ["https://porcalabs.github.io"],
         "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-        "allow_headers": ["Origin", "Content-Type", "Authorization"]
+        "allow_headers": [
+            "Origin",
+            "Content-Type",
+            "Authorization",
+            "Accept",
+            "X-Requested-With",
+            "Access-Control-Request-Method",
+            "Access-Control-Request-Headers",
+            "Access-Control-Allow-Methods"
+        ],
+        "expose_headers": [
+            "Access-Control-Allow-Origin",
+            "Access-Control-Allow-Methods",
+            "Access-Control-Allow-Headers"
+        ],
+        "supports_credentials": True,
+        "max_age": 3600
     }
 })
 
